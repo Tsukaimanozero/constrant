fb0c05cc66d3a1ddd9e697ebb6be9f3457ff37aa
Natthawat Witwatcharaphokhin <natthawat.w@cnes.co.th>
2022-10-09 14:17:05 +0700

    ipdate

--- constant.go
+++ constant.go
@@ -8,3 +8,12 @@ type TestConstrant struct {
 func TestPrintConstrant() string {
 	return "Hello"
 }
+func TestPrintConstrant2() string {
+	return "Hello"
+}
+
+// 1. git add .
+// 2. git commit -m "Add initail project"
+// 3. git push origin master
+// 4. git tag v0.0.1
+// 5. git push --tag origin master
